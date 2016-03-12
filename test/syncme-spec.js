3fc9ad543f2ed9a07dc36141856844e277596c95
zamirdan <zamirdan@gmail.com>
2016-03-13 00:16:40 +0200

    Update syncme-spec.js

--- test/syncme-spec.js
+++ test/syncme-spec.js
@@ -12,12 +12,24 @@ describe('syncme', function () {
     it('has function run', () => la(check.fn(syncme.run), 'syncme should be an object', syncme));
     it('has function sync', () => la(check.fn(syncme.sync), 'syncme should be an object', syncme));
     it('is using fs async methods as sync methods', (done) => testSyncFS(syncme,done));
+});
+
+describe('syncme.createSyncFunction', function () {
+    var syncme;
+    beforeEach(function () {
+        syncme = require('../syncme');
+    });
+    it('is converting async functions to sync', (done) => testcreateSyncFunction(syncme,done));
 
 });
 
+function checkResult(result){
+    if(result.error){
+        throw new Error (result.error);
+    }
+}
 
-
-function testSyncFS(syncme,done){
+    function testSyncFS(syncme,done){
 
     function* syncFile (){
 
@@ -46,13 +58,51 @@ function testSyncFS(syncme,done){
        done();
 
     }
-    function checkResult(result){
-        if(result.error){
-            throw new Error (result.error);
-        }
-    }
+
 
     syncme.run(syncFile());
 
+
+}
+
+
+function testcreateSyncFunction(syncme,done) {
+
+    var writeFile = syncme.createSyncFunction(fs.writeFile);
+    var stat = syncme.createSyncFunction(fs.stat);
+    var readFile = syncme.createSyncFunction(fs.readFile);
+    var unlink = syncme.createSyncFunction(fs.unlink);
+
+    function* syncFile (){
+
+        var timesStamp  = new Date().getMilliseconds()
+        var textToWrite = "test me "+ timesStamp;
+        var fileName = 'test/syncme'+timesStamp+'.txt';
+
+        var FileCreateResult = yield writeFile(fileName , textToWrite);
+        checkResult(FileCreateResult);
+
+
+        var fileStatResult = yield stat(fileName);
+        checkResult(FileCreateResult);
+
+        var readFileResult = yield readFile( fileName);
+        checkResult(readFileResult);
+
+        var textFromFile = readFileResult.result.toString();
+        if(textToWrite != textFromFile){
+            throw  new Error("test fail text is not match");
+        }
+
+        var removeFileResult = yield unlink( fileName);
+        checkResult(removeFileResult);
+
+        done();
+
+
+
+    }
+
+    syncme.run(syncFile());
 
 }
